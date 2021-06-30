58cb88733ecc484db797ca6d2ccded4b12311d7f
Quddus1916 <quddusjunior1916@gmail.com>
2021-06-29 21:05:34 -0500

    bootstrap

---
+++ my-app2/src/mycomponents/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function Myfunction(){
+    return(
+        <button className="btn btn-primary m-5">test bootstrap</button>
+    )
+}
+
+export default Myfunction
